3d47c2b31ddece4d718b2ecb422e7221a45a1673
ricard-gi <ricard.hernandez.gi@gmail.com>
2024-02-09 20:18:09 +0100

    selector

--- src/TriaIdioma.jsx
+++ src/TriaIdioma.jsx
@@ -3,20 +3,29 @@ import {useContext} from 'react';
 import Contexte from './Contexte';
 
 
-function TriaIdioma(){
+function Dropdown() {
+  const {idioma, setIdioma} = useContext(Contexte);
 
-    const {idioma, setIdioma} = useContext(Contexte);
+  const handleChange = (event) => {
+    setIdioma(event.target.value);
+  };
 
-
-    return (
-        <>
-
-        <button disabled={idioma=="ca"} className="p-4 border mr-4" onClick={()=>setIdioma("ca")} >CA</button>
-        <button disabled={idioma=="es"} className="p-4 border" onClick={()=>setIdioma("es")} >ES</button>
-        
-        </>
-    )
+  return (
+    <div className="w-32">
+      <label htmlFor="language" className="block text-sm font-medium text-gray-700">Idioma:</label>
+      <select
+        id="language"
+        name="language"
+        className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+        value={idioma}
+        onChange={handleChange}
+      >
+        <option value="ca">Català</option>
+        <option value="es">Espanyol</option>
+      </select>
+    </div>
+  );
 }
 
-export default TriaIdioma;
+export default Dropdown;
 
